d5a6d0b0a728d978423dbe30384f2f62cbcdb3c2
Nolwazi Masondo <masondop96@gmail.com>
2025-06-15 21:37:18 +0200

    Created a simple quiz

--- quiz.js
+++ quiz.js
@@ -1,5 +1,19 @@
-function sayHello(userName) {
-  console.log(`Hi ${userName}, how are you?`);
-}
+document.addEventListener("DOMContentLoaded", function () {
+  function checkAnswer() {
+    const correctAnswer = "4";
 
-sayHello("Lwazi");
+    const userAnswer = document.querySelector(
+      'input[name="quiz"]:checked'
+    ).value;
+    const feedback = document.getElementById("feedback");
+
+    if (userAnswer === correctAnswer) {
+      feedback.innerHTML = "Correct! Well done.";
+    } else {
+      feedback.innerHTML = "That's incorrect. Try again!";
+    }
+  }
+
+  const submitAnswer = document.getElementById("submit-answer");
+  submitAnswer.addEventListener("click", checkAnswer);
+});
